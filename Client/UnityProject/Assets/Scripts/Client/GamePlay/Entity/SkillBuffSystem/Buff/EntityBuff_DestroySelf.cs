84ed4c4931aeafb7a617ba0054d3015c4174bc26
Xue Bingsheng <xuebingsheng007@gmail.com>
2021-01-28 20:12:37 +0800

    V0.071 暴雪箱子、TNT箱子合成

---
+++ Client/UnityProject/Assets/Scripts/Client/GamePlay/Entity/SkillBuffSystem/Buff/EntityBuff_DestroySelf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[Serializable]
+public class EntityBuff_DestroySelf : EntityBuff
+{
+    protected override string Description => "毁灭自己";
+
+    public override void OnAdded(Entity entity)
+    {
+        base.OnAdded(entity);
+        if (!entity.IsNotNullAndAlive()) return;
+        entity.PassiveSkillMarkAsDestroyed = true;
+    }
+
+    protected override void ChildClone(EntityBuff newBuff)
+    {
+        base.ChildClone(newBuff);
+        EntityBuff_DestroySelf buff = ((EntityBuff_DestroySelf) newBuff);
+    }
+}
---
+++ Client/UnityProject/Assets/Scripts/Client/GamePlay/Entity/SkillBuffSystem/PassiveSkill/Executions/EntityPassiveSkillAction_DestroySelf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+[Serializable]
+public class EntityPassiveSkillAction_DestroySelf : EntityPassiveSkillAction, EntityPassiveSkillAction.IPureAction
+{
+    protected override string Description => "毁灭自己";
+
+    public void Execute()
+    {
+        Entity.PassiveSkillMarkAsDestroyed = true;
+    }
+
+    protected override void ChildClone(EntityPassiveSkillAction newAction)
+    {
+        base.ChildClone(newAction);
+        EntityPassiveSkillAction_DestroySelf action = ((EntityPassiveSkillAction_DestroySelf) newAction);
+    }
+
+    public override void CopyDataFrom(EntityPassiveSkillAction srcData)
+    {
+        base.CopyDataFrom(srcData);
+        EntityPassiveSkillAction_DestroySelf action = ((EntityPassiveSkillAction_DestroySelf) srcData);
+    }
+}
