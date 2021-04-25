75a7e6405a7bca3a08226730265dbc7387317895
Xue Bingsheng <xuebingsheng007@gmail.com>
2021-04-26 00:02:23 +0800

    open world adjustment

--- Client/UnityProject/Assets/Scripts/Client/GamePlay/World/OpenWorld/OpenWorldMapGenerators/MapGenerator.cs
+++ Client/UnityProject/Assets/Scripts/Client/GamePlay/World/OpenWorld/OpenWorldMapGenerators/MapGenerator.cs
@@ -254,7 +254,7 @@ public abstract class MapGenerator
                         data.WorldGP = appear_world;
                         data.LocalGP = appear_local;
                         data.EntityData.EntityOrientation = GridPosR.RotateOrientationClockwise90(data.EntityData.EntityOrientation, (int) staticLayoutOrientation);
-                        m_OpenWorld.WorldMap_TriggerEntityDataMatrix[worldGP.x, worldGP.y - Height, worldGP.z] = data;
+                        m_OpenWorld.WorldMap_TriggerEntityDataMatrix[data.WorldGP.x, data.WorldGP.y - Height, data.WorldGP.z] = data;
                     }
 
                     if (staticLayoutLayerData.DeterminePlayerBP && m_OpenWorld.InitialPlayerBP == GridPos3D.Zero)
